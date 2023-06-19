dab14b3984cd93e657cea70d1066b23e7b0c713a
Saurabh Bansal <saurabhbansal2443@gmail.com>
2023-06-19 18:02:58 +0530

    bubble sort

---
+++ BubbleSort.java
@@ -0,0 +1,21 @@
+import java.util.Arrays;
+
+public class BubbleSort{
+    public static void main(String[] args) {
+        int[] arr={4,7,0,1,2,3,9,5,6,8};
+
+        for(int i=0;i<arr.length-1;i++){
+
+            for(int j=i+1;j<arr.length;j++){
+
+                if( arr[i]>arr[j]){
+                    int temp = arr[i];
+                    arr[i]= arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+        System.out.println(Arrays.toString(arr));
+    }
+}
